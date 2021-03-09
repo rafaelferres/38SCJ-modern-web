6c6487d7bffd9f6efd3a1761512330fe8b98bddd
Rafael Ferrés <rafael.ferres-jum@parceiros-kroton.com.br>
2021-03-09 15:03:11 -0300

    Setup project - Next, TS, Tailwind and PostCSS

---
+++ pages/_app.tsx
@@ -0,0 +1,11 @@
+import '../styles/globals.css'
+
+import { AppProps } from 'next/app'
+
+import '../styles/index.css'
+
+function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
+
+export default App
